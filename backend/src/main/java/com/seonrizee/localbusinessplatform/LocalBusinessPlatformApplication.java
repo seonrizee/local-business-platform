594d845b3b7eb28157a04610a22207fa352603ef
maldoneee <maldoneee@gmail.com>
2024-11-12 21:14:44 +0900

    chore: 스프링 부트 프로젝트 생성 및 초기 설정

---
+++ backend/src/main/java/com/seonrizee/localbusinessplatform/LocalBusinessPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.seonrizee.localbusinessplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBusinessPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBusinessPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seonrizee/localbusinessplatform/LocalBusinessPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seonrizee.localbusinessplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBusinessPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
